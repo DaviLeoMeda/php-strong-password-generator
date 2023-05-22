1d751a0457bacd176644372004af7329fb6cef95
DaviLeoMeda <davileo.meda@gmail.com>
2023-05-22 17:20:34 +0200

    Second Step - structure completed

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <title>Strong Password</title>
 </head>
 <body>
-    <div class="bg-dark">
+    <div class="bg-dark py-5">
         <header class="text-center text-white">
             <h1>
                 Strong Password Generator
@@ -25,8 +25,50 @@
             <h5 class="p-4">No valid parameters entered</h5>
         </div>
 
-        <div class="bg-white text-center container rounded-2">
+        <div class="bg-white container rounded-2">
+            <div class="row justify-content-between m-3 py-4">
+                <label class="col-3 " for="">Password Lenght:</label>
+                <input class="col-3 rounded-1"  type="text">
+            </div>
+
+            <div class="row justify-content-between m-3 py-4">
+                <label class="col-4 " for="">Allow repeatings of one or more characters:</label>
+                <div class="col-3">
+
+                    <div>
+                        <label for="">Yes</label>
+                        <input type="radio" name="repeat" id="repeatYes" value="repeatY">
+                    </div>
+
+                    <div>
+                        <label for="">No</label>
+                        <input type="radio" name="repeat" id="repeatNo" value="repeatN">
+                    </div>
+                    
+                    <div class="my-3">
+                        <section>
+                            <input type="checkbox" name="words" id="words" value="words">
+                        <label for="">Words</label>
+                        </section>
+                        <section>
+                            <input type="checkbox" name="numbers" id="numbers" value="numbers">
+                        <label for="">Numbers</label>
+                        </section>
+                        <section>
+                            <input type="checkbox" name="symbols" id="symbols" value="symbols">
+                        <label for="">Symbols</label>
+                        </section>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="m-3 py-4">
+                <button class="btn btn-primary">Send</button>
+                <button class="btn btn-secondary">Cancel</button>
+            </div>
             
+
         </div>
 
     </div>
