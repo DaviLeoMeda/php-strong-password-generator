bb2671da0a4330949f6868a0e4230cdb4acec588
DaviLeoMeda <davileo.meda@gmail.com>
2023-05-24 19:43:06 +0200

    semi completed

--- include/partials/functions.php
+++ include/partials/functions.php
@@ -2,7 +2,7 @@
 session_start();
 
 
-$lenght = 0;
+$lenght = '';
 $lenghtFiltered = 0;
 $qtyLetters = ( isset($_GET['lenght'])) ? intval( $_GET['lenght']) : '';
 $repeatCha =  ( isset($_GET['repeat']))? $_GET['repeat'] : '';
@@ -29,4 +29,34 @@ $arrayComponents = [
 ];
 
 var_dump($arrayComponents);
+
+foreach ($arrayComponents as $elem){
+    if( $elem['active']){
+        $lenght .= $elem['base'];
+        $lenghtFiltered = strlen($elem['base']);
+    }
+}
+
+function createPassword($qtyLetters, $password, $repeatCha, $lenghtFiltered, $lenght ){
+    if( $qtyLetters > 0  && $lenghtFiltered > 0){
+        if ( $repeatCha == 'repeatNo'){
+            for ( $i = 0; strlen($password) < $qtyLetters; $i++ ){
+                $randomCha = $lenght[rand(0, strlen($lenght) - 1)];
+
+                if(!preg_match( "/$randomCha/", $password )){
+                    $password .= $randomCha;
+                }
+            }  
+        } else {
+            for ( $i = 0; strlen($password) < $qtyLetters; $i++ ){
+                $password .= $lenght[rand(0, strlen($lenght) - 1)];
+            }
+
+        }
+
+        return $password;
+    }
+}
+
+var_dump( createPassword($qtyLetters, $password, $repeatCha, $lenghtFiltered, $lenght ) );
 ?>
